4dd49f18d3d2fab0db00e27a8356ba688d0b8278
xnmk <xnmk_zhan@163.com>
2022-04-17 14:54:47 +0800

    完善了生成注册码功能

--- src/main/java/me/xnmk/community/controller/LoginController.java
+++ src/main/java/me/xnmk/community/controller/LoginController.java
@@ -1,8 +1,11 @@
 package me.xnmk.community.controller;
 
+import com.google.code.kaptcha.Producer;
 import me.xnmk.community.entity.User;
 import me.xnmk.community.enumeration.ActivationStates;
 import me.xnmk.community.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,6 +13,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Map;
 
 /**
@@ -20,8 +30,12 @@ import java.util.Map;
 @Controller
 public class LoginController {
 
+    private static Logger logger = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private UserService userService;
+    @Autowired
+    private Producer kaptchaProducer;
 
     /**
      * 跳转至注册页面
@@ -88,5 +102,28 @@ public class LoginController {
             model.addAttribute("target", "/index");
         }
         return "/site/operate-result";
+    }
+
+    /**
+     * 生成验证码
+     *
+     * @param response 响应
+     * @param session  会话
+     */
+    @GetMapping("/kaptcha")
+    public void getKaptcha(HttpServletResponse response, HttpSession session) {
+        // 生成验证码
+        String text = kaptchaProducer.createText();
+        BufferedImage image = kaptchaProducer.createImage(text);
+        // 将验证码存入Session（用于验证）
+        session.setAttribute("kaptcha", text);
+        // 将图片输出给浏览器
+        response.setContentType("image/png");
+        try {
+            OutputStream outputStream = response.getOutputStream();
+            ImageIO.write(image, "png", outputStream);
+        } catch (IOException e) {
+            logger.error("响应验证码失败：" + e.getMessage());
+        }
     }
 }
